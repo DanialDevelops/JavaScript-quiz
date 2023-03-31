2e27a0ec50b4066669c119aa651bb9fac59b8d38
DanialDevelops <danialchou101@gmail.com>
2023-03-31 14:24:51 -0400

    finished up questions and other functions all that remains is correcting my local storage

--- script.js
+++ script.js
@@ -6,21 +6,65 @@ var answerBtn = document.getElementById("answer-buttons")
 var questionEl = document.getElementById("question")
 var startBtn = document.getElementById("start-btn");
 var timerEl = document.getElementById("time")
+var initialsForm = document.getElementById("initals-form")
+var initialsInput = document.getElementById("initials-input")
+var submitBtn = document.getElementById("submit-btn")
 var mixedQuestions, currentQuestion
-var timeLeft = 10;
+var timeLeft = 60;
 var timeInterval;
 
 
 
 
 function startQuiz(){
+    score = 0;
     timer()
     questionContainer.classList.remove("hide");
     startContainer.classList.add("hide")
     mixedQuestions = questions.sort(() => Math.random() - .5)
-        currentQuestion = 0
-        nextQuestion()
+    currentQuestion = 0
+    nextQuestion()
 }
+
+function selectAnswer(e){
+    var selectedBtn = e.target
+    var correct = selectedBtn.dataset.correct
+    if(correct){
+        score +=10;
+    }else {
+        timeLeft -=10;
+        if(timeLeft < 0){
+            timeLeft = 0;
+        }
+    }
+    nextQuestion()
+}
+
+function nextQuestion(){
+    currentQuestion++
+    if (currentQuestion < mixedQuestions.length){
+    displayQuestion(mixedQuestions[currentQuestion])
+    } else {
+        endGame()
+    }
+    
+}
+
+function displayQuestion(question){
+    questionEl.innerText = question.question
+    answerBtn.innerHTML = "";
+    question.answers.forEach((answer) => {
+        var button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerBtn.appendChild(button)
+    })
+}
+
 
 
 function timer(){
@@ -33,36 +77,31 @@ function timer(){
     }, 1000)
 }
 
-function selectAnswer(e){
-
-}
-
-timeLeft = timeLeft-5
-
-function nextQuestion(){
-    displayQuestion(mixedQuestions[currentQuestion])
-    console.log(questions.choices[0])
-}
-
-function displayQuestion(question){
-    questionEl.innerText = question.question
-    questions.answers.forEach(answer => {
-        var button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        
-    })
-}
-
 
 
 function endGame(){
     clearInterval(timeInterval);
+    questionContainer.classList.add("hide")
+    endContainer.classList.remove("hide")
+    var scoreEl = document.getElementById("score")
+    if (scoreEl) {
+    scoreEl.textContent = score;
+    }
 }
+submitBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    var initials = initialsInput.value.trim()
+    if(initials !== ""){
+        var highScores = JSON.parse(localStorage.getItem("highScores")) || []
+        var newScore = {initials: initials, score: score }
+        highScores.push(newScore)
+        localStorage.setItem("highScores" , JSON.stringify(highScores))
+        window.location.href = "highscores.html"
+    }
+})
+
+
+startBtn.addEventListener("click", startQuiz);
 
 var questions = [
     {
@@ -73,36 +112,33 @@ var questions = [
             {text:  "quotes", correct: false},
             {text: "parenthesis", correct: false},
         ]
-    }
+    },
+    {
+        question: "Arrays in JavaScript can be used to store",
+        answers: [
+          { text: "numbers and strings", correct: false },
+          { text: "other arrays", correct: false },
+          { text: "booleans", correct: false },
+          { text: "all of the above", correct: true },
+        ]
+      },
+      {
+        question: "Commonly used data types DO NOT include:",
+        answers: [
+          { text: "strings", correct: false },
+          { text: "booleans", correct: false },
+          { text: "alerts", correct: true },
+          { text: "numbers", correct: false },
+        ]
+      },
+      {
+        question: "The condition in an if/else statement is enclosed with",
+        answers: [
+          { text: "quotes", correct: false },
+          { text: "curly brackets", correct: false },
+          { text: "parenthesis", correct: true },
+          { text: "square brackets", correct: false },
+        ]
+      },
 ]
-
-
-
-
-startBtn.addEventListener("click", startQuiz);
-// function countdown() {
-//    
-  
-//     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-//     
-//       // As long as the `timeLeft` is greater than 1
-//       if (timeLeft > 1) {
-//         // Set the `textContent` of `timerEl` to show the remaining seconds
-//         timerEl.textContent = timeLeft + ' seconds remaining';
-//         // Decrement `timeLeft` by 1
-//         timeLeft--;
-//       } else if (timeLeft === 1) {
-//         // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-//         timerEl.textContent = timeLeft + ' second remaining';
-//         timeLeft--;
-//       } else {
-//         // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-//         timerEl.textContent = '';
-//         // Use `clearInterval()` to stop the timer
-//         clearInterval(timeInterval);
-//         // Call the `displayMessage()` function
-//         displayMessage();
-//       }
-//     }, 1000);
-//   }
   
